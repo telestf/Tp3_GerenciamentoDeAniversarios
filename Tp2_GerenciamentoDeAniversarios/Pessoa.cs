d88894782ca7238c7831b2394fcce9560f338cf9
Thyago Teles <telesthyago@gmail.com>
2020-05-31 16:31:21 -0300

    Add project files.

---
+++ Tp2_GerenciamentoDeAniversarios/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tp3_GerenciamentoDeAniversarios
+{
+    class Pessoa
+    {
+        public string Nome;
+        public string Sobrenome;
+        public DateTime DataNascimento;
+
+        public Pessoa(string nome, string sobrenome, DateTime dataNascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataNascimento = dataNascimento;
+        }
+
+        public void CalcularAniversario()
+        {
+            DateTime comemoracaoAniversario = new DateTime(DateTime.Now.Year, DataNascimento.Month, DataNascimento.Day);
+            TimeSpan contagemDias = comemoracaoAniversario - DateTime.Now;
+            if(contagemDias.Days > 0)
+            {
+                Console.WriteLine($"Falta(m) {contagemDias.Days} dia(s) para o aniversário deste ano.");
+            }
+            else if (contagemDias.Days == 0)
+            {
+                Console.WriteLine("Hoje é o dia do aniversário.");
+            }
+            else
+            {
+                Console.WriteLine($"O aniversário deste ano foi há {contagemDias.Days} dia(s).");
+            }
+
+        }
+    }
+}
---
+++ Tp2_GerenciamentoDeAniversarios/PessoaRep.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tp3_GerenciamentoDeAniversarios
+{
+    class PessoaRep
+    {
+        private static List<Pessoa> Pessoas { get; set; }
+
+        public static void PesquisarPessoa()
+        {
+            if (Pessoas.Count >= 1)
+            {
+                Console.Write("Digite o nome ou parte do nome de quem você busca:");
+                var busca = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\r\nNão há pessoas na lista.\r\n");
+            }
+
+        }
+
+        public static List<Pessoa> AdicionarPessoa()
+        {
+            Pessoas = new List<Pessoa>();
+
+            Console.WriteLine("\r\nVocê escolheu **ADICIONAR NOVA PESSOA*\r\n");
+
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
+            Console.Write("Sobrenome: ");
+            var sobrenome = Console.ReadLine();
+            Console.Write("Data de Nascimento (dd/mm/yyyy): ");
+            var dataNascimento = Console.ReadLine();
+
+            if (DateTime.TryParse(dataNascimento, out DateTime parsedData))
+            {
+                Console.WriteLine("\r\nDeseja adicionar essa pessoa?\r\n");
+                Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+                Console.WriteLine($"Data do Nascimento: {parsedData} \r\n");
+
+                int opcao;
+
+                Console.Write("1 - Sim\r\n" +
+                        "0 - Não\r\n" +
+                        "\r\nEscolha uma opção: ");
+
+                if (int.TryParse(Console.ReadLine(), out opcao) && opcao == 0 || opcao == 1)
+                {
+                    switch (opcao)
+                    {
+                        case 1:
+                            Pessoa p = new Pessoa(nome, sobrenome, DateTime.Parse(dataNascimento));
+                            Pessoas.Add(p);
+                            Console.WriteLine("\r\nDados adicionados com sucesso!\r\n");
+                            break;
+
+                        case 0:
+                            Console.WriteLine();
+                            AdicionarPessoa();
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\r\nFormato incorreto de data! Tente novamente.\r\n");
+            }
+            return Pessoas;
+        }
+    }
+}
